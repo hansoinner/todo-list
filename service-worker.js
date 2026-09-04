7cacad66cce64dac038985af6d6ab2cef878ce7f
Sondev <137429521+hansoinner@users.noreply.github.com>
2026-09-04 20:50:27 +0200

    Add service worker for offline support

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = "todo-list-v1";
+const APP_SHELL = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./drag.css",
+    "./history.css",
+    "./theme.css",
+    "./toast.css",
+    "./data-transfer.css",
+    "./shortcuts.css",
+    "./tags.css",
+    "./toast.js",
+    "./script.js",
+    "./edit.js",
+    "./drag.js",
+    "./history.js",
+    "./theme.js",
+    "./data-transfer.js",
+    "./shortcuts.js",
+    "./tags.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) return cachedResponse;
+
+            return fetch(event.request).then(networkResponse => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type === "opaque") {
+                    return networkResponse;
+                }
+
+                const responseClone = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+                return networkResponse;
+            });
+        })
+    );
+});
